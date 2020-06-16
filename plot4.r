97739defb856d0f159cc1fb96cd95731849fa7bd
João Pedro <49729323+jpspm@users.noreply.github.com>
2020-06-16 15:54:21 -0300

    Add files via upload

---
+++ plot4.r
@@ -0,0 +1,21 @@
+consum <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(consum) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subconsum <- subset(consum,consum$Date=="1/2/2007" | consum$Date =="2/2/2007")
+subconsum <- subset(consum,consum$Date=="1/2/2007" | consum$Date =="2/2/2007")
+subconsum$Date <- as.Date(subconsum$Date, format="%d/%m/%Y")
+subconsum$Time <- strptime(subconsum$Time, format="%H:%M:%S")
+subconsum[1:1440,"Time"] <- format(subconsum[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subconsum[1441:2880,"Time"] <- format(subconsum[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+
+par(mfrow=c(2,2))
+
+with(subconsum,{
+plot(subconsum$Time,as.numeric(as.character(subconsum$Global_active_power)),type="l",xlab="",ylab="Global Active Power (kilowatts)") 
+plot(subconsum$Time,as.numeric(as.character(subconsum$Voltage)), type="l",xlab="datetime",ylab="Voltage")
+plot(subconsum$Time,subconsum$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
+with(subconsum,lines(Time,as.numeric(as.character(Sub_metering_1))))
+with(subconsum,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
+with(subconsum,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
+legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex = 0.3)
+plot(subconsum$Time,as.numeric(as.character(subconsum$Global_reactive_power)),type="l",xlab="datetime",ylab="Global_reactive_power")
+})
